5fed36564f082252b7f01028086e3c4b97e1f8c8
BandersGit <andy.i.flaskan@gmail.com>
2020-08-26 10:57:16 +0200

    Initial commit

---
+++ Vscode Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Vscode_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.ReadLine();
+        }
+    }
+}
